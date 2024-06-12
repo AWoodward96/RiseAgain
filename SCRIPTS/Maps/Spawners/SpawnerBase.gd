edbb96e6838af943086d165bb17f524f479750d0
AWoodward96 <alex.woodward96@gmail.com>
2024-06-12 11:50:00 -0400

    Items and Stat rebalancing

--- SCRIPTS/Maps/Spawners/SpawnerBase.gd
+++ SCRIPTS/Maps/Spawners/SpawnerBase.gd
@@ -1,10 +1,18 @@
+@tool
 extends Node2D
 class_name SpawnerBase
 
 const PREFIX = "Spawn_"
+const NodeSize : int = 64
 
 @export var DebugName : String
-@export var Position: Vector2i
+@export var Position: Vector2i :
+	set(value):
+		Position = value
+		print("should update")
+		position = Position * NodeSize
+
+
 @export var Enabled : bool = true
 @export var Allegiance : GameSettings.TeamID = GameSettings.TeamID.ENEMY
 @export var AIBehavior : AIBehaviorBase
